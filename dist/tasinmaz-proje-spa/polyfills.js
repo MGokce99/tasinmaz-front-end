be9984bc804f09f0c3a44eccea821623824676c9
MGokce99 <melahatgokce1999@gmail.com>
2024-06-12 17:17:17 +0300

    Bug fix

--- dist/tasinmaz-proje-spa/polyfills.js
+++ dist/tasinmaz-proje-spa/polyfills.js
@@ -5800,8 +5800,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Gokce\source\repos\tasinmaz-proje-spa\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\Gokce\source\repos\tasinmaz-proje-spa\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Gokce\source\repos\tasinmaz-front-end\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Gokce\source\repos\tasinmaz-front-end\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
